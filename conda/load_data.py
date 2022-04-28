2b620655f6f8dc0591660c1ec7647ab80fc44a84
Michel Metran <michelmetran@gmail.com>
2022-04-27 23:36:36 -0300

    aa

--- conda/load_data.py
+++
@@ -1,9 +0,0 @@
-import os
-from setuptools.config import read_configuration
-
-setup_cfg = os.path.join(os.path.dirname('.'), 'setup.py')
-if os.path.isfile(setup_cfg):
-    for kwargs in read_configuration(setup_cfg).values():
-        #setup_cfg_data.update(kwargs)
-        print(kwargs)
-
